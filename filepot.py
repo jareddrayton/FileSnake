7f89d0fa1d8ecd2be25a945e0fa21499705eefa0
unknown <jareddrayton@users.noreply.github.com>
2019-09-11 11:58:56 +0100

    MVP will rename files in place.

--- filepot.py
+++ filepot.py
@@ -5,37 +5,85 @@ from natsort import natsorted
 
 # detect the current working directory
 path = os.getcwd()
+print("Current working directory:", path)
 
-show_id = ""
+# manually provide show ID
+show_id = "252322"
 
+show_name = "Hunter X Hunter"
+
+# request token stuff
 headers={'Content-type':'application/json', 'Accept':'application/json'}
-
 r = requests.post('https://api.thetvdb.com/login', data = '{"apikey":"I7KOIY59UWQL3JPS"}', headers=headers)
-
-print(r.json())
-print(type(r.json()))
-
+#print(r.json())
+# Extract the token from the returned json data
 token = r.json()['token']
-print(token)
+#print(token)
 
 # get seasons
-
-#curl -X GET --header 'Accept: application/json' --header 'Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NjgyMzE0NjksImlkIjoiRmlsZVBvdCIsIm9yaWdfaWF0IjoxNTY4MTQ1MDY5fQ.WAA5Q913bv5TfvWF9o1ShnP5UyooB1YYM-zTJeNg0YrWSl1Wx6WEJlRebWGuyd4JobCvrXj3MMIJcJPadhCPdV3AOVdVbeBQrl0ZHCZ9Ea2KD5ebgcFF1UBuUM5rU1gGo3vs8WQsJmQapdwyCf7ITs3J2GRASE8E3bdmkz-rzDSgXPMC-zPf4DZBvA9Ru5mn_8CmJXNxK73omNDrsnu0LMfcvKZMczproWYK9aQpMfvq40z-yn61TlE7Ybb3stQxWKAgFR6-4FftJUW-SKRbtAbIZAhpJ_wHbh0ahQjN0nU-L7ejDULb0qyvod5kY19AQcfrDZwjEjnPN02qBnHwkg' 'https://api.thetvdb.com/series/252322/episodes/summary'
 headers={'Content-type':'application/json', 'Authorization':'Bearer ' + token}
-
 b = requests.get('https://api.thetvdb.com/series/252322/episodes/summary', headers=headers)
-print(b.json())
+seasons = b.json()['data']['airedSeasons']
 
-print(b.json()['data']['airedSeasons'])
+# get episodes total
+file_hierarchy = {}
+# Loop over seasons starting from Season 1, missing out the specials season
+for i in range(1, len(seasons)):
+    episodes = requests.get('https://api.thetvdb.com/series/252322/episodes/query?airedSeason='+ str(i), headers=headers)
+    #print(episodes.json()['data'])
+    print(len(episodes.json()['data']))
+    file_hierarchy[str(i)] = len(episodes.json()['data'])
+
+print(file_hierarchy)
+
+#print(episodes.json()['data']['airedSeasons'])
+
 
 for root, dirs, files in os.walk("Test//."):
     #print(files)
-    #print(natsorted(files))
-    
     files = natsorted(files)
-    
     #print(files)
     
+    print("Total source files:", len(files))
+    
     #for filename in files:
-        
         #print(filename)
+
+total_episodes = 0
+
+for k, v in file_hierarchy.items():
+    print("Season", k, "Episodes", v)
+    total_episodes += v
+
+
+if len(files) == total_episodes:
+    print("Success")
+else:
+    print("Error: File count missmatch")
+
+
+new_names = []
+
+for k, v in file_hierarchy.items():
+    for i in range(1, v+1):
+        new_names.append(show_name + " s0" + k + "e" + str(i) + ".mkv")
+
+print(len(files))
+print(len(new_names))
+
+for i in range(len(files)):
+    print(files[i], new_names[i])
+    src = "Test//" + files[i]
+    dst = "Test//" + new_names[i]
+    
+    os.rename(src, dst) 
+
+#for filename in os.listdir("xyz"): 
+    #dst ="Hostel" + str(i) + ".jpg"
+    #src ='xyz'+ filename 
+    #dst ='xyz'+ dst 
+          
+    # rename() function will 
+    # rename all the files 
+    #os.rename(src, dst) 
+    #i += 1
