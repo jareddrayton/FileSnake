98be7f7ce43c4ebca9e73cfe87c30b561a69c70f
unknown <jareddrayton@users.noreply.github.com>
2019-10-13 17:00:27 +0100

    Implemented basic series search

--- filepot.py
+++ filepot.py
@@ -28,46 +28,54 @@ parser.add_argument("-id", "--showid",
 
 args = parser.parse_args()
 
-source = Path(args.source)
+print(args.source)
+
+source = args.source.strip('" \\ .')
+print(source)
+
 
 show_id = args.showid
 
 append_episode_names = True
 
-# manually provide show Name
+#manually provide show Name
 show_name = "Hunter X Hunter"
 
 ###########################################################################
-
-print(source)
-
-
 # Generate test data
-generate_test_data.usecase1_test(args.source)
+
+generate_test_data.usecase1_test(source)
+
+source = Path(source)
+
+###########################################################################
+# Print the current working directory
+print("Current working directory:", Path.cwd())
 
 
-
-# detect the current working directory
-path = Path.cwd()
-print("Current working directory:", path)
-
-
-def name_search():
-    # try and find the name from the directory name
-    # if show id is not provided, search for 
-    
-    pass
-
-
-
-
+###########################################################################
 # request a token from the TVDB
 headers={'Content-type':'application/json', 'Accept':'application/json'}
 r = requests.post('https://api.thetvdb.com/login', data = '{"apikey":"I7KOIY59UWQL3JPS"}', headers=headers)
 token = r.json()['token']
 
 
+###########################################################################
 
+def series_search():
+    # try and find the name from the directory name
+    # if show id is not provided, search for 
+    
+    headers={'Content-type':'application/json', 'Authorization':'Bearer ' + token}
+    search_results = requests.get('https://api.thetvdb.com/search/series?name=' + requests.utils.quote(args.source), headers=headers)
+    print(search_results.json())
+    
+    for i in range(len(search_results.json()['data'])):
+        print(search_results.json()['data'][i]["seriesName"], search_results.json()['data'][i]["id"]) 
+
+series_search()
+
+###########################################################################
 
 # Create a dictionary with season as key and number of episodes as value.
 def create_file_hierarchy():
@@ -86,7 +94,7 @@ def create_file_hierarchy():
         file_hierarchy[str(i)] = len(episodes.json()['data'])
         
         for i in range(len(episodes.json()['data'])):
-            episode_names.append(episodes.json()['data'][i]["episodeName"].strip('?'))
+            episode_names.append(episodes.json()['data'][i]["episodeName"].strip('?*."/\\[]:;|,'))
 
     return file_hierarchy, episode_names 
 
--- generate_test_data.py
+++ generate_test_data.py
@@ -1,6 +1,12 @@
 import shutil
 from pathlib import Path
 
+
+# Tests to write
+# mixed filetypes e.g. .avi and mkv
+
+
+
 file_type = ".mkv"
 
 episodes = 148
@@ -16,5 +22,5 @@ def usecase1_test(show_name):
     
     for i in range(1, episodes + 1):
         s = Path(show_name + str(i) + file_type)
-        path =  Path.cwd() / p / s
+        path = Path.cwd() / p / s
         Path(path).touch()
